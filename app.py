ba9326f3ff20dcf4fd1053906d310ea0aca7629f
joshva2104 <49963261+joshva2104@users.noreply.github.com>
2023-10-17 12:59:43 -0600

    added tabs for wave 1

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+from dash.dependencies import Input, Output
+
 from dotenv import load_dotenv
 import os
 
@@ -18,15 +20,15 @@ logo1_url = "https://picsum.photos/seed/picsum/200/300"  # Replace with the URL 
 logo2_url = "https://picsum.photos/seed/picsum/200/300"  # Replace with the URL of your second logo image
 
 nav_items = [
-    dbc.NavItem(dbc.NavLink("Wave 1", href="#", className="navbar-item active")),
-    dbc.NavItem(dbc.NavLink("Wave 2", href="#", className="navbar-item active")),
-    dbc.NavItem(dbc.NavLink("Wave 3", href="#", className="navbar-item active")),
+    dbc.NavItem(dbc.NavLink("Wave 1", href="#")),
+    dbc.NavItem(dbc.NavLink("Wave 2", href="#")),
+    dbc.NavItem(dbc.NavLink("Wave 3", href="#")),
     dbc.DropdownMenu(
         label="Cross Wave Analysis",  # Dropdown label
         children=[
-            dbc.DropdownMenuItem("Option 4", href="#", className="navbar-item"),
-            dbc.DropdownMenuItem("Option 5", href="#", className="navbar-item"),
-            dbc.DropdownMenuItem("Option 6", href="#", className="navbar-item"),
+            dbc.DropdownMenuItem("Option 4", href="#"),
+            dbc.DropdownMenuItem("Option 5", href="#"),
+            dbc.DropdownMenuItem("Option 6", href="#"),
         ],
     ),
 ]
@@ -108,8 +110,49 @@ wave_1_demographic_figures = html.Div([
                 ], className="row-container"),
         ])
 
+wave_1_tab2_figures = html.Div([
+            html.Div([
+                dcc.Graph(id="figure-1", className="figure"),
+                dcc.Graph(id="figure-2", className="figure"),
+                ], className="row-container"),
+            html.Div([
+                dcc.Graph(id="figure-3", className="figure"),
+                dcc.Graph(id="figure-4", className="figure"),
+                ], className="row-container"),
+        ])
+
+wave_1_tab3_figures = html.Div([
+            html.Div([
+                dcc.Graph(id="figure-1", className="figure"),
+                dcc.Graph(id="figure-2", className="figure"),
+                ], className="row-container"),
+            html.Div([
+                dcc.Graph(id="figure-3", className="figure"),
+                dcc.Graph(id="figure-4", className="figure"),
+                ], className="row-container"),
+        ])
 
 
+wave_1_figure_groups = {
+    "wave-1-demographics-tab" : wave_1_demographic_figures,
+    "tab-2" : wave_1_tab2_figures,
+    "tab-3" : wave_1_tab3_figures,
+}
+
+wave_1_tabs = html.Div(
+    [
+        dbc.Tabs([
+            dbc.Tab(label="Demographics", id="wave-1-demographics-tab", className="nav-item"),
+            dbc.Tab(label="Tab 2", id="tab-2", className="nav-item"),
+            dbc.Tab(label="Tab 3", id="tab-3", className="nav-item"),
+        ],
+            id="wave-1-tabs",
+            active_tab="wave-1-demographics-tab",
+            className="nav-fill justify-content-center",
+        ),
+        html.Div(id="content"),
+    ]
+)
 
 
 app.layout = html.Div([
@@ -121,12 +164,22 @@ app.layout = html.Div([
         src='/assets/script.js'  # Path to your JavaScript file
     ),  
     Navbar,
-    filter_options,
     html.Div([
-        html.H1("Welcome to Your Dash App", style={"text-align": "center", "color": text_color}),  # Use the hex color code for text color
-        wave_1_demographic_figures,
-    ])
+        html.H1("Welcome to Your Dashboard", style={"text-align": "center", "color": text_color}),  # Use the hex color code for text color
+    ]),
+    filter_options,
+    wave_1_tabs,
 ])
+
+
+@app.callback(Output("content", "children"), [Input("wave-1-tabs", "active_tab")])
+def update_wave_1_tab(selected_tab):
+    print(selected_tab)
+    if selected_tab == "tab-1":
+        return wave_1_figure_groups["tab-2"]
+    elif selected_tab == "tab-2":
+        return wave_1_figure_groups["tab-3"]
+    return wave_1_figure_groups["wave-1-demographics-tab"]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
