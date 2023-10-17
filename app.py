52e642dcde1c08aa736b0e8ff5e1777ccab20828
joshva2104 <49963261+joshva2104@users.noreply.github.com>
2023-10-17 08:02:35 -0600

    added filters to layout

--- app.py
+++ app.py
@@ -28,18 +28,78 @@ nav_items = [
     dbc.NavItem(html.Img(src=logo2_url, height="50px")),
 ]
 
+filter_options = html.Div([
+    html.Div([
+        html.Label("Filter 1"),
+        dcc.Dropdown(
+            id="filter-1",
+            options=[
+                {"label": "Option 1", "value": "option-1"},
+                {"label": "Option 2", "value": "option-2"},
+                {"label": "Option 3", "value": "option-3"},
+                {"label": "Select All", "value": "select-all"},
+            ],
+            multi=True,
+        ),
+    ], className="filter"),
+    
+    html.Div([
+        html.Label("Filter 2"),
+        dcc.Dropdown(
+            id="filter-2",
+            options=[
+                {"label": "Option 1", "value": "option-1"},
+                {"label": "Option 2", "value": "option-2"},
+                {"label": "Option 3", "value": "option-3"},
+                {"label": "Select All", "value": "select-all"},
+            ],
+            multi=True,
+        ),
+    ], className="filter"),
+    
+    html.Div([
+        html.Label("Filter 3"),
+        dcc.Dropdown(
+            id="filter-3",
+            options=[
+                {"label": "Option 1", "value": "option-1"},
+                {"label": "Option 2", "value": "option-2"},
+                {"label": "Option 3", "value": "option-3"},
+                {"label": "Select All", "value": "select-all"},
+            ],
+            multi=True,
+        ),
+    ], className="filter"),
+    
+    html.Div([
+        html.Label("Filter 4"),
+        dcc.Dropdown(
+            id="filter-4",
+            options=[
+                {"label": "Option 1", "value": "option-1"},
+                {"label": "Option 2", "value": "option-2"},
+                {"label": "Option 3", "value": "option-3"},
+                {"label": "Select All", "value": "select-all"},
+            ],
+            multi=True,
+        ),
+    ], className="filter"),
+], className="filter-container")
+
+
 
 app.layout = html.Div([
     html.Link(
         rel='stylesheet',
         href='/assets/custom.css'  # Path to your custom CSS file
-    ),
+        ),
     dbc.NavbarSimple(
         children=nav_items,
         brand="CO-CEAL DASHBOARD",
         brand_href="#",
         style={"background-color": navbar_bg_color, "color": text_color},
-    ),
+        ),
+    filter_options,
     html.Div([
         html.H1("Welcome to Your Dash App", style={"text-align": "center", "color": text_color}),  # Use the hex color code for text color
         html.Div([
